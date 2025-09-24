3fd5ce674054fabfcd2b32e4c4aca00f83d0baf8
Allegretti77 <vini16.allegretti@gmail.com>
2025-09-24 19:10:41 +0000

    Leitura Inicial (Matriz e valores)

--- src/main.c
+++ src/main.c
@@ -1,11 +1,46 @@
-// a
+#include <stdlib.h>
+
 void encontrar(int n,int m, char mapa[][]){
   
 }
 
 
 int main(){
-  int n, m;
+  int n;
+  //Inicio leitura e malloc
+  printf("Insira o tamanho da matriz [N]x[N] : ");
   scanf("%d", &n);
-  char mapa[n][m]; //recebe ou H ou V
+  printf("\n");
+
+  char **matrix = (char **)malloc(n * sizeof(char *));
+  if(matrix == NULL) return 1;
+
+  for(int i = 0; i < n; i++){
+    matrix[i] = (char *)malloc(n * sizeof(char)); 
+    if(matrix[i] == NULL) return 1;
+  }
+
+  printf("Digite %d linhas de %d caracteres:\n", n, n);
+  for (int i = 0; i < n; i++) {
+    scanf("%s", matrix[i]);
+  }
+  
+  int lin, col;
+  printf("Linha e coluna inicial: ");
+  scanf("%d  %d", &lin, &col);
+  //Fim da leitura da matrix
+
+
+
+
+
+
+
+
+
+  for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+  free(matrix);
+
 }
